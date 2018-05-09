5dcac475a17015d2f69563a23198132d38460b0d
Hutten <m.hutten@student.tue.nl>
2018-05-09 10:56:38 +0200

    Created ASA class

---
+++ code/Algorithms/PackASA.java
@@ -0,0 +1,13 @@
+
+public class ASA implements PackerStrategy{
+    
+    @Override
+    public RectanglesContainer pack(ProblemStatement PS){
+        RectanglesContainer RC = new RectanglesContainer();
+        
+        
+        
+        return RC;
+    }
+    
+}
