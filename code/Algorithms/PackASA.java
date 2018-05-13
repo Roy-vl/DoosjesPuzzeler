0a13188fe5bd22cd164187e2ee5fbfa548f1db20
MHutten <32169483+MHutten@users.noreply.github.com>
2018-05-13 11:29:41 +0200

    Worked in the implementation of RandomLS in packASA

--- code/Algorithms/PackASA.java
+++ code/Algorithms/PackASA.java
@@ -1,5 +1,6 @@
 import java.lang.Math;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class PackASA implements PackerStrategy{
 
@@ -10,6 +11,9 @@ double[] widths = new double[wMax-wMin];
 double[] heights = new double[wMax-wMin];
 double[] fr = new double[wMax-wMin];
 double[] iter = new double[wMax-wMin];
+boolean first = true;
+ArrayList R = new ArrayList(); // sequence of rectangles
+ArrayList newR = new ArrayList(); // new sequence of rectangles
 
 //Calculate the set of candidate widths W
 public void CandidateWidths(){ 
@@ -30,9 +34,33 @@ public void SortByFillingRatio(){
 
 }
 //RandomLS procedure
-public int RandomLS(){
-    int minimumheight; //minimum found height
- return minimumheight;  
+public int RandomLS(ArrayList R, double Wk, int iter){
+    int minimumHeight; //minimum found height
+    int currentHeight; //current found height
+    ArrayList sr; // ArrayList of sorting rules on Wk
+    
+    if (first) {
+        first = false;
+        for(/** for each sorting rule */) {
+            // Sort R
+            minimumHeight = heuristicPacking(R, Wk);
+            // add sorting rule to sr
+        }
+        //Sort sr in decreasing order of the heigth returned by heuristicPacking
+    }
+    // Select the ith sorting rule from sr randomly
+    // Sort R using this rule
+    minimumHeight = heuristicPacking(R, Wk);
+    for (int i = 1; i <= iter; i++) {
+        // Generate sequence newR from R by randomly swapping two rectangles
+        currentHeight = heuristicPacking(newR, Wk);
+        if (currentHeight <= minimumHeight) {
+            minimumHeight = currentHeight;
+            R = newR;
+        }
+    }
+    // Update sr to make sure it is sorted in decreasing order of height
+    return minimumHeight;  
 }
 
     @Override
