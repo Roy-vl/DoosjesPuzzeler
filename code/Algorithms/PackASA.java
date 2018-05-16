c6cdce8cae3161e1ea75a5307660e69655b058ff
MHutten <32169483+MHutten@users.noreply.github.com>
2018-05-16 11:54:01 +0200

    More updates to randomLS

--- code/Algorithms/PackASA.java
+++ code/Algorithms/PackASA.java
@@ -1,6 +1,7 @@
 import java.lang.Math;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PackASA implements PackerStrategy{
 
@@ -15,6 +16,7 @@ public class PackASA implements PackerStrategy{
     ArrayList newR = new ArrayList(); // new sequence of rectangles
     PackerStrategy heuristicPacking = new PackTetris();
     RectanglesContainer container;
+    Random random = new Random();
 
     //Calculate the set of candidate widths W
     public void candidateWidths(){ 
@@ -41,11 +43,11 @@ public class PackASA implements PackerStrategy{
         int currentWidth; // current found width
         Rectangle[] rectangles = PS.getRectangles(); // Array of rectangles
         Rectangle[] newOrder; // new order in rectangles
-        Rule[] sortingRules; // ArrayList of sorting rules on Wk
+        Rule[][] sortingRules; // ArrayList of sorting rules on Wk
 
         if (first) {
             first = false;
-            for (Rule r: sortingRules) {
+            for (Rule[] r: sortingRules) {
                 // Sort rectangles
                 minimumWidth = getContainerWidth(PS, rectangles);
                 r.width = minimumWidth;
@@ -55,10 +57,41 @@ public class PackASA implements PackerStrategy{
         
         
         // Select the ith sorting rule from sortingRules randomly
+        for (int i = 0; i < sortingRules.length; i++) {
+            Rule[i][2] = (2*i)/(sortingRules.length*(sortingRules.length+1));
+        }
+        
+        int totalSum = 0;
+        
+        for (Rule[] r: sortingRules) {
+            totalSum += r[2];
+        }
+        
+        int index = random.nextInt(totalSum);
+        int sum = 0;
+        int i = 0;
+        while (sum < index) {
+            sum += Rule[i++][2];
+        }
+        
+        Rule[] chosen = Rule[Math.max(0, i-1)][];
+        
         // Sort rectangles using this rule
         minimumWidth = getContainerWidth(PS, rectangles);
         for (int i = 1; i <= iter; i++) {
-            // Generate sequence newOrder from rectangels by randomly swapping two rectangles
+            int j = -1;
+            int k = -1;
+            Rectangle save = new Rectangle();
+            
+            while (j == k) {
+                j = random.nextInt(rectangles.length);
+                k = random.nextInt(rectangles.length);
+            }
+            
+            save = rectangles[j];
+            rectangles[j] = rectangles[k];
+            rectangles[k] = save;
+            
             currentWidth = getContainerWidth(PS, rectangles);
             if (currentWidth <= minimumWidth) {
                 minimumWidth = currentWidth;
@@ -68,6 +101,12 @@ public class PackASA implements PackerStrategy{
         // Update sortingRules to make sure it is sorted in decreasing order of height
         return minimumWidth;  
     }
+    
+    
+    int r = (new Random()).nextInt(8)+3;
+    
+    Arrays.sort(rectangles, new SortByDecreasingWidth());
+    Collections.sort(//)
     
     /** Solves problem statement with new array and returns width. */
     public int getContainerWidth(ProblemStatement PS, Rectangle[] rectangles) {
