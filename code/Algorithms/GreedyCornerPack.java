843355d8cddc41fe5e1740e24dca3394902aa46d
Gijs <gijsleb@hotmail.com>
2018-05-30 11:03:23 +0200

    removed blah

--- code/Algorithms/GreedyCornerPack.java
+++ code/Algorithms/GreedyCornerPack.java
@@ -111,9 +111,9 @@ public class GreedyCornerPack implements PackerStrategy{
             }
             
             if(!placed){
-                System.out.println("RECTANGLE "+curRec.id+" COULD NOT BE PLACED BY PLACING IT IN A CORNER REVERTING TO OTHER (SLOW) METHODS");
-                System.out.println("Rectangle dimensions :"+curRec.getWidth()+","+curRec.getHeight());
-                System.out.println("Container dimensions :"+width+","+height);
+                //System.out.println("RECTANGLE "+curRec.id+" COULD NOT BE PLACED BY PLACING IT IN A CORNER REVERTING TO OTHER (SLOW) METHODS");
+                //System.out.println("Rectangle dimensions :"+curRec.getWidth()+","+curRec.getHeight());
+                //System.out.println("Container dimensions :"+width+","+height);
                 
                 //basically just tries to find the first free spot in leftness order (just as in GreedyTrivialPack)
                 Point P = corners.get(0);
@@ -128,7 +128,7 @@ public class GreedyCornerPack implements PackerStrategy{
                         P.y = 0;
                     }
                 }            
-                System.out.println("RECTANGLE "+curRec.id+" COULD BE PLACED BY REVERTING TO OTHER (SLOW) METHODS");
+                //System.out.println("RECTANGLE "+curRec.id+" COULD BE PLACED BY REVERTING TO OTHER (SLOW) METHODS");
             }
         }
         
