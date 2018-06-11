a921090d63ccfba8b5497cb37ffd0e89d7413a55
Roy <royvanleeuwen@live.nl>
2018-06-11 13:22:57 +0200

    more experimenting

--- code/Algorithms/AutoSelectPack.java
+++ code/Algorithms/AutoSelectPack.java
@@ -1,34 +1,55 @@
 
+public class AutoSelectPack implements PackerStrategy {
 
-public class AutoSelectPack implements PackerStrategy{
-    
     @Override
-    public boolean applicable(ProblemStatement PS){
+    public boolean applicable(ProblemStatement PS) {
         return true;
     }
- 
-    @Override
-    public RectanglesContainer pack(ProblemStatement PS){
 
-        if(PS.getContainerHeight()==0){
-            return new MultipleGreedyTrivialBestFitPack().pack(PS);
-        }else{
-            return new GreedyTrivialBestFitPack().pack(PS);
-        }
-        /*
-        if(PS.getRectangleAmount()<25){
+    @Override
+    public RectanglesContainer pack(ProblemStatement PS) {
+
+        if (PS.getRectangleAmount() < 25) {
             System.out.println("Choosed BackTrackCornerPack");
             return new BacktrackCornerPack().pack(PS);
-        }     
-        
-        if(PS.getContainerHeight()>0){
-            System.out.println("Choosed GreedyCornerPack");
-            return new GreedyCornerPack().pack(PS);
-
         }
-        
-        System.out.println("Choosed MultipleGreedyCornerPack");
-        return new MultipleGreedyCornerPack().pack(PS);
-*/
+
+        if (PS.getRectangleAmount() == 25) {
+            if (PS.getContainerHeight() == 0) {
+                System.out.println("Choosed MultipleGreedyTriviaBestFitPack");
+                RectanglesContainer RCOne = new MultipleGreedyTrivialBestFitPack().pack(PS);
+                
+                return RCOne;
+                /*
+                RectanglesContainer RCTwo = new MultipleGreedyCornerPack().pack(PS);
+                if (RCOne.getCost() > RCTwo.getCost()) {
+                    return RCTwo;
+                } else {
+                    return RCOne;
+                }
+                 */
+            } else {
+                System.out.println("Choosed GreedyTriviaBestFitPack");
+                RectanglesContainer RCOne = new GreedyTrivialBestFitPack().pack(PS);
+                
+                return RCOne;
+                /*
+                sRectanglesContainer RCTwo = new GreedyCornerPack().pack(PS);
+                if (RCOne.getCost() > RCTwo.getCost()) {
+                    return RCTwo;
+                } else {
+                    return RCOne;
+                }
+                 */
+            }
+        }
+
+        if (PS.getContainerHeight() == 0) {
+            System.out.println("Choosed MultipleGreedyCornerPack");
+            return new MultipleGreedyCornerPack().pack(PS);
+        }
+
+        System.out.println("Choosed GreedyCornerPack");
+        return new GreedyCornerPack().pack(PS);
     }
 }
--- code/Algorithms/GreedyCornerBestFitPack.java
+++ code/Algorithms/GreedyCornerBestFitPack.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class GreedyCornerPack implements PackerStrategy{
+public class GreedyCornerBestFitPack implements PackerStrategy{
     RectanglesContainer RC;
     
     int width;
